e4eeb31328652ee23fa11051cc7dfe29d516c173
justyna <j.pokrzywinska@gmail.com>
2019-06-05 21:26:37 +0200

    add linkedlist + tests

--- data-structures/linked-list/LinkedList.js
+++ data-structures/linked-list/LinkedList.js
@@ -1,10 +1,78 @@
-class LinkedList {
+export class LinkedList {
     constructor() {
-        // TODO jaki array skoro to linked lista???
-        this.list = {};
+        this.size = 0;
+        this.first = new Element();
     }
 
     add(elem) {
+        if (this.size === 0) {
+            this.first = new Element(elem);
+            this.size++;
+            return;
+        }
+        let temp = this.first;
+        while (temp.next) {
+            temp = temp.next;
+        }
+        temp.next = new Element(elem);
+        this.size++;
+    }
 
+    addAt(index, elem) {
+        if (index >= this.size) {
+            throw new Error("Index out of bound");
+        }
+        let temp = this.first;
+        for (let i = 1; i <= index; i++) {
+            temp = temp.next;
+        }
+        temp.value = elem;
+    }
+
+    get(index) {
+        if (index >= this.size) {
+            throw new Error("Index out of bound");
+        }
+        let temp = this.first;
+        for (let i = 1; i <= index; i++) {
+            temp = temp.next;
+        }
+        return temp.value;
+    }
+
+    remove(index) {
+        if (index >= this.size) {
+            throw new Error("Index out of bound");
+        }
+        let temp = this.first;
+        for (let i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+        let before = temp;
+        let actual = temp.next;
+        before.next = actual.next;
+        this.size--;
+    }
+
+    removeObj(obj) {
+        let temp = this.first;
+        for (let i = 0; i < this.size; i++) {
+            if (temp.value === obj) {
+                let actual = temp;
+                before.next = actual.next;
+                this.size--;
+                return;
+
+            }
+            let before = temp;
+            temp = temp.next;
+        }
+    }
+}
+
+
+class Element {
+    constructor(value) {
+        this.value = value;
     }
 }
---
+++ data-structures/linked-list/LinkedList.spec.js
@@ -0,0 +1,147 @@
+import {assert, expect, should} from "chai";
+import {LinkedList} from "./LinkedList";
+
+describe('linkedList test', () => {
+    it('shoud successfully add element to list', () => {
+
+        //given
+        const list = new LinkedList();
+        const el = 5;
+
+        //when
+        list.add(el);
+
+        //then
+        const size = 1;
+        expect(list.size).to.eql(size);
+
+    });
+});
+
+describe('linkedList test', () => {
+    it('shoud successfully add 2 elements to list', () => {
+
+        //given
+        const list = new LinkedList();
+        const el = 5;
+        const el2 = 4;
+
+        //when
+        list.add(el);
+        list.add(el2);
+
+        //then
+        const size = 2;
+        expect(list.size).to.eql(size);
+
+    });
+});
+
+describe('linkedList test', () => {
+    it('shoud successfully add 3 elements to list', () => {
+
+        //given
+        const list = new LinkedList();
+        const el = 5;
+        const el2 = 4;
+        const el3 = 3;
+
+        //when
+        list.add(el);
+        list.add(el2);
+        list.add(el3);
+
+        //then
+        const size = 3;
+        expect(list.size).to.eql(size);
+
+    });
+});
+
+describe('linkedList test', () => {
+    it('shoud successfully get element with index', () => {
+
+        //given
+        const list = new LinkedList();
+        const el = 5;
+        const el2 = 4;
+        const el3 = 3;
+
+        //when
+        list.add(el);
+        list.add(el2);
+        list.add(el3);
+
+        //then
+        expect(list.get(0)).to.eql(el);
+        expect(list.get(1)).to.eql(el2);
+        expect(list.get(2)).to.eql(el3);
+    });
+});
+
+describe('linkedList test', () => {
+    it('shoud successfully add element on given index', () => {
+
+        //given
+        const list = new LinkedList();
+        const el = 5;
+        const el2 = 4;
+        const el3 = 3;
+        const newElem = 10;
+        list.add(el);
+        list.add(el2);
+        list.add(el3);
+
+        //when
+        const index = 1;
+        list.addAt(index, newElem);
+
+        //then
+        expect(list.get(1)).to.eql(newElem);
+    });
+});
+
+describe('linkedList test', () => {
+    it('shoud successfully remove element on given index', () => {
+
+        //given
+        const list = new LinkedList();
+        const el = 5;
+        const el2 = 4;
+        const el3 = 3;
+        list.add(el);
+        list.add(el2);
+        list.add(el3);
+
+        //when
+        const index = 1;
+        list.remove(index);
+
+        //then
+        const expectedSize = 2;
+        expect(list.size).to.eql(expectedSize);
+        expect(list.get(1)).to.eql(el3);
+    });
+});
+
+describe('linkedList test', () => {
+    it('shoud successfully remove element with given value', () => {
+
+        //given
+        const list = new LinkedList();
+        const el = 1;
+        const el2 = 2;
+        const el3 = 3;
+        list.add(el);
+        list.add(el2);
+        list.add(el3);
+
+        //when
+        list.removeObj(el2);
+
+        //then
+        const expectedSize = 2;
+        expect(list.size).to.eql(expectedSize);
+        expect(list.get(1)).to.eql(el3);
+    });
+});
